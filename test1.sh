9f60be1907cebffcde61647c9c199f09e1ffa231
John Kelly <john.kelly@stfc.ac.uk>
2016-06-10 13:17:23 +0100

    This is a test commit. I have added a function and called it a few times.

--- test1.sh
+++ test1.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 echo "This is a test"
 echo "fools are fools"
+
+function catman {
+	# this is a sub -- whatever that is
+	echo "I am in  a sub"
+	sleep 2
+}
+
+# more stuff
+echo fat 
+catman
+sleep 3
+catman
+echo "The sea is blue and far away"
+
+exit 0
+
